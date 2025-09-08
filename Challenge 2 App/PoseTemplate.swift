04421660c039e58d329cc0437b1a598cb223987b
Meagan <meagansimyh@gmail.com>
2025-09-08 09:04:47 +0800

    included pose template, edited max and min player count

---
+++ Challenge 2 App/PoseTemplate.swift
@@ -0,0 +1,18 @@
+//
+//  PoseTemplate.swift
+//  Challenge 2 App
+//
+//  Created by Meagan Sim on 8/9/25.
+//
+
+import SwiftUI
+
+struct PoseTemplate: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PoseTemplate()
+}
---
+++ Challenge 2 App/SoloPoseTemplates.swift
@@ -0,0 +1,18 @@
+//
+//  SoloPoseTemplates.swift
+//  Challenge 2 App
+//
+//  Created by Meagan Sim on 8/9/25.
+//
+
+import SwiftUI
+
+struct SoloPoseTemplates: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SoloPoseTemplates()
+}
