25da6fe675595b3c7282df9738134f07a40eca8b
Meagan <meagansimyh@gmail.com>
2025-09-13 01:36:41 +0800

    gameview, maintenance view

---
+++ Challenge 2 App/UIViews/MaintenanceView.swift
@@ -0,0 +1,18 @@
+//
+//  MaintenanceView.swift
+//  Challenge 2 App
+//
+//  Created by Meagan Sim on 13/9/25.
+//
+
+import SwiftUI
+
+struct MaintenanceView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MaintenanceView()
+}
